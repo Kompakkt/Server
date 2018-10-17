5b7b99302c03ba06f645caa33b240ec0fd2a33bb
Kai Niebes <kai.niebes@outlook.com>
2018-10-17 18:46:02 +0200

    Reduced calls to MongoDB ObjectsRepository collection

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -1,9 +1,12 @@
 import { MongoClient, ObjectId } from 'mongodb';
 import { Configuration } from './configuration';
+import { Verbose } from '../environment';
+import { inspect as InspectObject } from 'util';
 
 const Mongo = {
     Client: undefined,
     Connection: undefined,
+    DBObjectsRepository: undefined,
     initClient: () => {
         this.Client = new MongoClient(`mongodb://${Configuration.Mongo.Hostname}:${Configuration.Mongo.Port}/`, { useNewUrlParser: true });
     },
@@ -11,7 +14,6 @@ const Mongo = {
         this.Connection.then(() => {
             const db = this.Client.db(Configuration.Mongo.Databases.ObjectsRepository.Name.toLowerCase());
 
-            // Init collections
             Configuration.Mongo.Databases.ObjectsRepository.Collections.forEach(collection => {
                 db.createCollection(collection.toLowerCase());
             });
@@ -20,29 +22,48 @@ const Mongo = {
     establishConnection: () => {
         this.Connection = this.Client.connect();
     },
+    initObjectsRepository: () => {
+        this.Connection.then(() => {
+            this.DBObjectsRepository = this.Client.db(Configuration.Mongo.Databases.ObjectsRepository.Name);
+        });
+    },
     addToObjectCollection: (request, response) => {
         this.Connection.then(() => {
-            console.log(request.params.collection.toLowerCase());
-            console.log(request.body);
+            if (Verbose) {
+                console.log('VERBOSE: Adding the following document to collection ' + request.params.collection);
+                console.log(request.params.collection.toLowerCase());
+                console.log(InspectObject(request.body));
+            }
 
-            const db = this.Client.db('objectsrepository');
-            const collection = db.collection(request.params.collection.toLowerCase());
+            const collection = this.DBObjectsRepository.collection(request.params.collection.toLowerCase());
 
             collection.insertOne(request.body, (db_error, result) => {
                 response.send(result.ops);
-                console.log(result.ops);
+
+                if (Verbose) {
+                    console.log('VERBOSE: Success! Added the following');
+                    console.log(result.ops);
+                }
             });
         });
     },
     addMultipleToObjectCollection: (request, response) => {
         this.Connection.then(() => {
-            console.log(request.body);
-            response.send(request.body);
+            if (Verbose) {
+                console.log('VERBOSE: Adding the following document to collection ' + request.params.collection);
+                console.log(request.params.collection.toLowerCase());
+                console.log(InspectObject(request.body));
+            }
 
-            const db = this.Client.db('objectsrepository')
-            const collection = db.collection(request.params.collection)
+            const collection = this.DBObjectsRepository.collection(request.params.collection.toLowerCase());
 
-            // collection.insertMany(request.body)
+            collection.insertMany(request.body, (db_error, result) => {
+                response.send(result.ops);
+                if (Verbose) {
+                    console.log('VERBOSE: Success! Added the following');
+                    console.log(result.ops);
+                }
+            });
         });
     },
     addToAccounts: (collection, data) => {
@@ -50,17 +71,17 @@ const Mongo = {
     },
     getFromObjectCollection: (request, response) => {
         this.Connection.then(() => {
-            const db = this.Client.db(Configuration.Mongo.Databases.ObjectsRepository.Name.toLowerCase());
-            const _collection = db.collection(request.params.collection.toLowerCase());
-            _collection.findOne({ '_id': new ObjectId(request.params.identifier) }, (db_error, result) => {
+            const collection = this.DBObjectsRepository.collection(request.params.collection.toLowerCase());
+
+            collection.findOne({ '_id': new ObjectId(request.params.identifier) }, (db_error, result) => {
                 response.send(result);
             });
         });
     },
     getAllFromObjectCollection: (request, response) => {
         this.Connection.then(() => {
-            const db = this.Client.db(Configuration.Mongo.Databases.ObjectsRepository.Name.toLowerCase());
-            const collection = db.collection(request.params.collection.toLowerCase());
+            const collection = this.DBObjectsRepository.collection(request.params.collection.toLowerCase());
+            
             collection.find({}).toArray((db_error, result) => {
                 response.send(result);
             });
@@ -71,5 +92,6 @@ const Mongo = {
 Mongo.initClient();
 Mongo.establishConnection();
 Mongo.initCollections();
+Mongo.initObjectsRepository();
 
 export { Mongo };
