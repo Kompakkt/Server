f6d9c4292e690836c155a09368bcd58e40264ce8
Kai Niebes <kai.niebes@outlook.com>
2018-11-21 18:08:38 +0100

    Shorten Configuration import to Conf for line length

--- src/services/express.ts
+++ src/services/express.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { RootDirectory } from '../environment';
-import { Configuration } from './configuration';
+import { Configuration as Conf } from './configuration';
 import * as bodyParser from 'body-parser';
 import * as corser from 'corser';
 
@@ -11,19 +11,19 @@ import * as HTTPS from 'https';
 const Express = {
     server: express(),
     startListening: () => {
-        if (Configuration.Express.enableHTTPS) {
-            const privateKey = readFileSync(Configuration.Express.SSLPaths.PrivateKey);
-            const certificate = readFileSync(Configuration.Express.SSLPaths.Certificate);
+        if (Conf.Express.enableHTTPS) {
+            const privateKey = readFileSync(Conf.Express.SSLPaths.PrivateKey);
+            const certificate = readFileSync(Conf.Express.SSLPaths.Certificate);
 
-            HTTPS.createServer({key: privateKey, cert: certificate}, Server).listen(Configuration.Express.Port);
-            console.log(`HTTPS Server started and listening on port ${Configuration.Express.Port}`);
+            HTTPS.createServer({key: privateKey, cert: certificate}, Server).listen(Conf.Express.Port);
+            console.log(`HTTPS Server started and listening on port ${Conf.Express.Port}`);
         } else {
-            HTTP.createServer(Server).listen(Configuration.Express.Port);
-            console.log(`HTTP Server started and listening on port ${Configuration.Express.Port}`);
+            HTTP.createServer(Server).listen(Conf.Express.Port);
+            console.log(`HTTP Server started and listening on port ${Conf.Express.Port}`);
         }
 
-        /*Server.listen(Configuration.Express.Port, () => {
-            console.log(`Server started and listening on port ${Configuration.Express.Port}`);
+        /*Server.listen(Conf.Express.Port, () => {
+            console.log(`Server started and listening on port ${Conf.Express.Port}`);
         });*/
     }
 };
@@ -37,10 +37,10 @@ Server.use(bodyParser.json());
 // TODO: Find out which routes need CORS
 Server.use(corser.create());
 // Static
-if (Configuration.Uploads.createSubfolders) {
-    Server.use('/models', express.static(`${RootDirectory}/${Configuration.Uploads.UploadDirectory}/${Configuration.Uploads.subfolderPath}`));
+if (Conf.Uploads.createSubfolders) {
+    Server.use('/models', express.static(`${RootDirectory}/${Conf.Uploads.UploadDirectory}/${Conf.Uploads.subfolderPath}`));
 } else  {
-    Server.use('/models', express.static(`${RootDirectory}/${Configuration.Uploads.UploadDirectory}`));
+    Server.use('/models', express.static(`${RootDirectory}/${Conf.Uploads.UploadDirectory}`));
 }
 
 export { Express, Server };
