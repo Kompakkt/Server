e9db80da3fb5ffc4bf9075c1a90a8518b53f0c6c
Kai Niebes <kai.niebes@outlook.com>
2018-12-19 17:22:49 +0100

    Update interfaces

--- src/interfaces/compilation.interface.ts
+++ src/interfaces/compilation.interface.ts
@@ -3,6 +3,8 @@ import { Model } from './model.interface';
 export interface Compilation {
     _id?: string;
     name?: string;
+    description?: string;
     relatedOwner?: string;
+    passcode?: string;
     models: Model[];
 }
