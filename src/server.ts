52e13a6b31a9999b64c7b77039b7489fa2add129
Kai Niebes <kai.niebes@outlook.com>
2019-01-15 22:45:50 +0100

    Check auth on all routes

--- src/server.ts
+++ src/server.ts
@@ -18,35 +18,38 @@ if (isMaster) {
     // GET
     // Find document by ID in collection
     // http://localhost:8080/api/v1/get/find/Person/5bbf023850c06f445ccab442
-    Server.get('/api/v1/get/find/:collection/:identifier', Mongo.getFromObjectCollection);
+    Server.get('/api/v1/get/find/:collection/:identifier', Mongo.checkAccount, Mongo.getFromObjectCollection);
     // Return all documents of a collection
-    Server.get('/api/v1/get/findall/:collection', Mongo.getAllFromObjectCollection);
+    Server.get('/api/v1/get/findall/:collection', Mongo.checkAccount, Mongo.getAllFromObjectCollection);
+    // Return data linked to currently logged in LDAP Account
+    Server.get('/api/v1/get/ldata', Mongo.checkAccount, Mongo.getLinkedData);
+
     // POST
     // Post single document to collection
     // http://localhost:8080/api/v1/post/push/person/
-    Server.post('/api/v1/post/push/:collection', Mongo.addToObjectCollection);
+    Server.post('/api/v1/post/push/:collection', Mongo.checkAccount, Mongo.addToObjectCollection);
     // Post multiple documents to collection
-    Server.post('/api/v1/post/pushmultiple/:collection', Mongo.addMultipleToObjectCollection);
+    Server.post('/api/v1/post/pushmultiple/:collection', Mongo.checkAccount, Mongo.addMultipleToObjectCollection);
     // On user submit
-    Server.post('/api/v1/post/submit', Mongo.submit);
+    Server.post('/api/v1/post/submit', Mongo.checkAccount, Mongo.submit);
     // On Screenshot update
-    Server.post('/api/v1/post/screenshot/:identifier', Mongo.updateScreenshot);
+    Server.post('/api/v1/post/screenshot/:identifier', Mongo.checkAccount, Mongo.updateScreenshot);
 
     // Upload API
     // Upload a file to the server
-    Server.post('/upload', Upload.Multer.single('file'), Upload.UploadRequest);
+    Server.post('/upload', Mongo.checkAccount, Upload.Multer.single('file'), Upload.UploadRequest);
     // User signals that all necessary files are uploaded
     // TODO: Post Upload Cleanup
-    Server.post('/uploadfinished', Upload.UploadFinish);
+    Server.post('/uploadfinished', Mongo.checkAccount, Upload.UploadFinish);
     // User signals that upload was cancelled
-    Server.post('/uploadcancel', Upload.UploadCancel);
+    Server.post('/uploadcancel', Mongo.checkAccount, Upload.UploadCancel);
     // Metadata
-    Server.post('/addmetadata', Upload.Multer.single('file'), Upload.AddMetadata);
-    Server.post('/cancelmetadata', Upload.Multer.single('file'), Upload.CancelMetadata);
+    Server.post('/addmetadata', Mongo.checkAccount, Upload.Multer.single('file'), Upload.AddMetadata);
+    Server.post('/cancelmetadata', Mongo.checkAccount, Upload.Multer.single('file'), Upload.CancelMetadata);
 
     // Authentication
     Server.post('/login', Express.passport.authenticate('ldapauth', {session: true}), Mongo.addToAccounts);
-    Server.get('/auth', Mongo.checkAccount);
+    Server.get('/auth', Mongo.checkAccount, (req, res) => res.send({status: 'ok'}));
 
     Express.startListening();
 
