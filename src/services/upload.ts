1c12fd5b1d1a8e414ac9294fd29417f353385b82
Kai Niebes <kai.niebes@outlook.com>
2019-03-06 16:47:55 +0100

    Refactor upload of files to work better with multiple filetypes

--- src/services/upload.ts
+++ src/services/upload.ts
@@ -3,19 +3,14 @@ import { RootDirectory } from '../environment';
 import { Logger } from './logger';
 
 import { ensureDirSync, moveSync, pathExistsSync, removeSync, move, statSync } from 'fs-extra';
-import { dirname, extname, basename } from 'path';
+import { dirname, extname, basename, join } from 'path';
 import * as klawSync from 'klaw-sync';
 import * as multer from 'multer';
+import slugify from 'slugify';
 
 const Upload = {
   Multer: multer({
-    dest: `${RootDirectory}/${Configuration.Uploads.TempDirectory}`,
-    onFileUploadStart: function(file) {
-      Logger.info(file.originalname + ' is starting ...');
-    },
-    onFileUploadComplete: function(file) {
-      Logger.info(file.fieldname + ' uploaded to  ' + file.path);
-    }
+    dest: `${RootDirectory}/${Configuration.Uploads.TempDirectory}`
   }),
   AddMetadata: (request, response) => {
     const tempPath = `${request['file'].path}`;
@@ -43,10 +38,12 @@ const Upload = {
   },
   UploadRequest: (request, response) => {
     // TODO: Checksum
+    // TODO: Subdirectories based on request?
     const tempPath = `${request['file'].destination}/${request['file'].filename}`;
     let newPath = `${RootDirectory}/${Configuration.Uploads.UploadDirectory}/`;
     newPath += `${request.headers['semirandomtoken']}/`;
-    newPath += `${request.headers['relpath']}`;
+    newPath += `${request.headers['relpath']}/`;
+    newPath += `${Date.now()}_${slugify(request['file'].originalname)}`;
 
     ensureDirSync(dirname(newPath));
     moveSync(tempPath, newPath);
@@ -74,51 +71,59 @@ const Upload = {
     if (!pathExistsSync(path)) {
       response.json([]).end('Upload not finished');
     } else {
+      const found = klawSync(path);
+      // Move all files to top level
+      found.filter(file => !file.stats.isDirectory())
+      .forEach(file => {
+        const src = file.path;
+        const filename = basename(file.path);
+        const dest = join(path, filename);
+        moveSync(src, dest);
+      });
+      // Remove subdirs
+      found.filter(file => file.stats.isDirectory())
+      .sort((a, b) => a.path.length - b.path.length)
+      .forEach(directory => removeSync(directory.path));
+
       const foundFiles = klawSync(path);
-
-      // TODO: remove nested top directories until a file is top-level
-      // TODO: Add more type cases for image, audio, video
-      if (Type === 'model') {
-        const modelExt = [
-          '.ply',
-          '.obj',
-          '.babylon'
-        ];
-
-        const modelFiles = await foundFiles.filter(file => {
-          return modelExt.indexOf(extname(file.path)) !== -1;
-        }); // .map(file => file.path.substr(file.path.indexOf('models/')));
-
-        const ResponseFile = {
-          file_name: '',
-          file_link: '',
-          file_size: 0,
-          file_format: ''
-        };
-        if (modelFiles.length > 0) {
-          const ResponseFiles = modelFiles.map(modelFile => {
-            const result = { ...ResponseFile };
-            result.file_format = extname(modelFile.path);
-            let _relativePath = modelFile.path.replace(RootDirectory, '');
-            _relativePath = (_relativePath.charAt(0) === '/') ? _relativePath.substr(1) : _relativePath;
-            result.file_link = `${_relativePath}`;
-            result.file_name = `${basename(modelFile.path)}`;
-            result.file_size = parseInt(`${modelFile.stats.size}`, 10);
-            return result;
-          }).sort((a, b) => a.file_size - b.file_size);
-
-          Logger.info(ResponseFiles);
-
-          response.json(ResponseFiles);
-        } else {
-          response.json(foundFiles);
-        }
-      } else {
-        const resultFiles = await foundFiles.map(file => file.path.substr(file.path.indexOf('uploads/')));
-        response.json(resultFiles);
+      // TODO: Add more filters
+      const filter: string[] = [];
+      switch (Type) {
+        case 'model': filter.push('.ply', '.obj', '.babylon'); break;
+        default: break;
       }
-      response.end('Done!');
+
+      const filteredFiles = await foundFiles.filter(file => {
+        return filter.indexOf(extname(file.path)) !== -1;
+      }); // .map(file => file.path.substr(file.path.indexOf('models/')));
+
+      const ResponseFile = {
+        file_name: '',
+        file_link: '',
+        file_size: 0,
+        file_format: ''
+      };
+
+      const prepareResponseFiles = (fileArray: ReadonlyArray<klawSync.Item>) => {
+        return fileArray.map(file => {
+          const result = { ...ResponseFile };
+          result.file_format = extname(file.path);
+          let _relativePath = file.path.replace(RootDirectory, '');
+          _relativePath = (_relativePath.charAt(0) === '/') ? _relativePath.substr(1) : _relativePath;
+          console.log(_relativePath);
+          result.file_link = `${_relativePath}`;
+          result.file_name = `${basename(file.path)}`;
+          result.file_size = parseInt(`${file.stats.size}`, 10);
+          return result;
+        }).sort((a, b) => a.file_size - b.file_size);
+      };
+
+      const ResponseFiles = prepareResponseFiles((filteredFiles.length > 0)
+        ? filteredFiles : foundFiles);
+      Logger.info(ResponseFiles);
+      response.json(ResponseFiles);
     }
+    response.end('Done!');
   }
 };
 
