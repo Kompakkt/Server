6eb8e283e4d2c2eb5d5fdaa8fca2ce0cee55eee2
Kai Niebes <kai.niebes@outlook.com>
2019-04-18 13:35:45 +0200

    MailerService: Add user to Database containing information about the mail sent

--- src/services/mailer.ts
+++ src/services/mailer.ts
@@ -1,7 +1,9 @@
 import * as nodemailer from 'nodemailer';
+import { Db } from 'mongodb';
 
 import { Configuration } from './configuration';
 import { Logger } from './logger';
+import { Mongo } from './mongo';
 
 const Mailer = {
   isConfigValid: () => {
@@ -11,7 +13,6 @@ const Mailer = {
       && Configuration.Mailer.Target;
   },
   sendMail: async (request, response) => {
-
     if (!request.body || !Configuration.Mailer.Target[request.body.target]) {
       response.send({ status: 'error' });
       return false;
@@ -32,12 +33,39 @@ const Mailer = {
     transporter.sendMail(mailOptions)
       .then(success => {
         Logger.info(`Nodemailer sent mail:`, success);
+        Mailer.addUserToDatabase(request, true);
         response.send({ status: 'ok', message: 'Mail has been sent' });
       })
       .catch(error => {
         Logger.err(`Failed sending mail:`, error);
+        Mailer.addUserToDatabase(request, false);
         response.send({ status: 'error', message: 'Failed sending mail' });
       });
+  },
+  addUserToDatabase: async (request, mailSent) => {
+    const target = request.body.target;
+    if (!Object.keys(Configuration.Mailer.Target)
+      .includes(target)) return;
+
+    const AccDb: Db = await Mongo.getAccountsRepository();
+    const ldap = AccDb.collection('ldap');
+    const user = await ldap.findOne({ sessionID: request.sessionID });
+    const collection = AccDb.collection(target);
+
+    const subject = request.body.subject;
+    const mailbody = request.body.mailbody;
+    const document = {
+      target, content: { mailbody, subject },
+      timestamp: new Date().toISOString(),
+      user, answered: false, mailSent,
+    };
+
+    const insertResult = await collection.insertOne(document);
+    if (insertResult.result.ok !== 1) {
+      Logger.info(`Failed adding user to mail database`);
+    } else {
+      Logger.info(`Added user to DB ${document}`);
+    }
   },
 };
 
