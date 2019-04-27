3438654ed6c07f3bd85f9fb295317bbae4e68d85
Kai Niebes <kai.niebes@outlook.com>
2019-04-27 02:27:27 +0200

    Add TypeGuard service

---
+++ src/services/typeguards.ts
@@ -0,0 +1,19 @@
+import { IAnnotation, ICompilation, IModel } from '../interfaces';
+
+export const isCompilation = (obj: any): obj is ICompilation => {
+  const compilation = obj as ICompilation;
+  return compilation.models !== undefined
+    && compilation.name !== undefined
+    && compilation.description !== undefined;
+};
+
+export const isModel = (obj: any): obj is IModel => {
+  const model = obj as IModel;
+  return model.name !== undefined && model.mediaType !== undefined
+    && model.online !== undefined && model.finished !== undefined;
+};
+
+export const isAnnotation = (obj: any): obj is IAnnotation => {
+  const annotation = obj as IAnnotation;
+  return annotation.body !== undefined && annotation.target !== undefined;
+};
