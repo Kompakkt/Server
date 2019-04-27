eb011afa5ec2db919bb1ac84b32907a3c387dc32
Kai Niebes <kai.niebes@outlook.com>
2019-04-27 03:43:50 +0200

    Add isPerson TypeGuard and null-check in TypeGuards

--- src/services/typeguards.ts
+++ src/services/typeguards.ts
@@ -1,24 +1,29 @@
-import { IAnnotation, ICompilation, IModel, IMetaDataDigitalObject } from '../interfaces';
+import { IAnnotation, ICompilation, IMetaDataDigitalObject, IMetaDataPerson, IModel } from '../interfaces';
 
 export const isCompilation = (obj: any): obj is ICompilation => {
   const compilation = obj as ICompilation;
-  return compilation.models !== undefined
+  return compilation && compilation.models !== undefined
     && compilation.name !== undefined
     && compilation.description !== undefined;
 };
 
 export const isModel = (obj: any): obj is IModel => {
   const model = obj as IModel;
-  return model.name !== undefined && model.mediaType !== undefined
+  return model && model.name !== undefined && model.mediaType !== undefined
     && model.online !== undefined && model.finished !== undefined;
 };
 
 export const isAnnotation = (obj: any): obj is IAnnotation => {
   const annotation = obj as IAnnotation;
-  return annotation.body !== undefined && annotation.target !== undefined;
+  return annotation && annotation.body !== undefined && annotation.target !== undefined;
 };
 
 export const isDigitalObject = (obj: any): obj is IMetaDataDigitalObject => {
   const digobj = obj as IMetaDataDigitalObject;
-  return digobj.digobj_title !== undefined;
-}
+  return digobj && digobj.digobj_title !== undefined;
+};
+
+export const isPerson = (obj: any): obj is IMetaDataPerson => {
+  const person = obj as IMetaDataPerson;
+  return person && person.person_prename !== undefined && person.person_surname !== undefined;
+};
