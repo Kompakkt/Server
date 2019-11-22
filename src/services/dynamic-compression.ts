f9d2234ed76394406894f5b372fbced3192e24c1
Kai Niebes <kai.niebes@outlook.com>
2019-11-22 13:59:56 +0100

    Improve dynamic compression. Save compressed files for re-use

--- src/services/dynamic-compression.ts
+++ src/services/dynamic-compression.ts
@@ -1,7 +1,7 @@
-import { compress } from 'iltorb';
-import { exists, readFile } from 'fs-extra';
-import { join } from 'path';
 import { Request, Response } from 'express';
+import { exists, move, readFile, writeFile } from 'fs-extra';
+import { join } from 'path';
+import { brotliCompress, constants, gzip } from 'zlib';
 
 export const serveFile = (directory: string) => async (
   request: Request,
@@ -11,27 +11,15 @@ export const serveFile = (directory: string) => async (
   const acceptedEncodings = request.headers['accept-encoding'] as
     | string
     | undefined;
-  const doesFileExist = await new Promise<boolean>((resolve, _) =>
-    exists(filename, resolve),
-  );
+
+  const doesFileExist = await wrapperExists(filename);
 
   if (!doesFileExist) {
     response.sendStatus(404);
     return;
   }
 
-  const resultFile = await new Promise<Buffer>((resolve, reject) =>
-    readFile(filename, (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(data);
-    }),
-  ).catch(err => {
-    console.error(err);
-    return undefined;
-  });
+  const resultFile = await wrapperReadFile(filename);
 
   if (!resultFile) {
     response.sendStatus(404);
@@ -45,22 +33,90 @@ export const serveFile = (directory: string) => async (
 
   let compressedFile = resultFile;
 
-  // TODO: more encoding types
   if (acceptedEncodings.includes('br')) {
-    const result = await getBrotli(resultFile);
+    const compressedFilename = `${filename}.br`;
+    const doesCompressedExist = await wrapperExists(compressedFilename);
+    const result = doesCompressedExist
+      ? await wrapperReadFile(compressedFilename)
+      : await getBrotli(resultFile);
     if (result) {
       compressedFile = result;
       response.setHeader('Content-Encoding', 'br');
+      if (
+        !doesCompressedExist &&
+        !(await wrapperExists(`${compressedFilename}.tmp`))
+      ) {
+        saveCompressed(result, compressedFilename);
+      }
+    }
+  } else if (acceptedEncodings.includes('gzip')) {
+    const compressedFilename = `${filename}.gz`;
+    const doesCompressedExist = await wrapperExists(compressedFilename);
+    const result = doesCompressedExist
+      ? await wrapperReadFile(compressedFilename)
+      : await getGZip(resultFile);
+    if (result) {
+      compressedFile = result;
+      response.setHeader('Content-Encoding', 'gzip');
+      if (
+        !doesCompressedExist &&
+        !(await wrapperExists(`${compressedFilename}.tmp`))
+      ) {
+        saveCompressed(result, compressedFilename);
+      }
     }
   }
 
   response.send(compressedFile);
 };
 
+const wrapperExists = async (filename: string) =>
+  new Promise<boolean>((resolve, _) => exists(filename, resolve));
+
+const wrapperReadFile = async (filename: string) =>
+  new Promise<Buffer>((resolve, reject) =>
+    readFile(filename, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    }),
+  ).catch(err => {
+    console.error(err);
+    return undefined;
+  });
+
 const getBrotli = async (file: Buffer) =>
-  compress(file, { quality: 4 })
-    .then(result => result)
+  new Promise<Buffer | undefined>((resolve, reject) =>
+    brotliCompress(
+      file,
+      {
+        params: {
+          [constants.BROTLI_PARAM_QUALITY]: 4,
+        },
+      },
+      (err, res) => (err ? reject(undefined) : resolve(res)),
+    ),
+  );
+
+const getGZip = async (file: Buffer) =>
+  new Promise<Buffer | undefined>((resolve, reject) =>
+    gzip(file, { level: 6 }, (err, res) =>
+      err ? reject(undefined) : resolve(res),
+    ),
+  );
+
+const saveCompressed = async (file: Buffer, filename: string) => {
+  const tempName = `${filename}.tmp`;
+  return writeFile(tempName, file)
+    .then(() => {
+      return move(tempName, filename);
+    })
+    .then(() => {
+      console.log('Moved temp compressed file to real compressed file');
+    })
     .catch(err => {
-      console.error(err);
-      return undefined;
+      console.log('Failed saving compressed file', err);
     });
+};
