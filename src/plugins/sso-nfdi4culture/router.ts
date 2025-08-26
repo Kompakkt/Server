b4ab196fb05d3f9fed00c62da3f1856a10a9f8d9
Kai Niebes <kai.niebes@outlook.com>
2025-08-26 17:29:08 +0200

    SSO-NFDI4Culture: Update SAML router

--- src/plugins/sso-nfdi4culture/router.ts
+++ src/plugins/sso-nfdi4culture/router.ts
@@ -1,24 +1,76 @@
 import { SAML } from '@node-saml/node-saml';
-import Elysia from 'elysia';
+import Elysia, { t } from 'elysia';
 import configServer from 'src/server.config';
 import { getSAMLConfig } from './config';
 import { log } from 'src/logger';
+import { samlProfileToUser } from './saml';
+import { userCollection } from 'src/mongo';
+import { ObjectId } from 'mongodb';
 
 const ssoNFDI4CultureRouter = new Elysia()
   .use(configServer)
-  .get('/auth/saml/health', () => {
+  .get('/sso-nfdi4culture/saml/health', () => {
     return { status: 'OK', message: 'SAML authentication service is running' };
   })
-  .get('/auth/saml', async ({ redirect }) => {
+  .get('/sso-nfdi4culture/saml', async ({ redirect }) => {
     const samlService = new SAML(await getSAMLConfig());
     const authUrl = await samlService.getAuthorizeUrlAsync('', '', {});
     return redirect(authUrl);
   })
-  .post('/auth/saml/callback', async ({ redirect, body }) => {
-    const samlService = new SAML(await getSAMLConfig());
-    const result = await samlService.validatePostResponseAsync(body as any);
-    log(result, result?.profile?.getSamlResponseXml?.());
-    return redirect('/');
-  });
+  .post(
+    '/sso-nfdi4culture/saml/callback',
+    async ({ redirect, body, status, request, cookie: { auth }, jwt }) => {
+      const samlService = new SAML(await getSAMLConfig());
+      const result = await samlService.validatePostResponseAsync(body);
+      if (!result || !result.profile) return status(401, 'Invalid SAML response');
+
+      const user = samlProfileToUser(result.profile);
+      if (!user) return status(401, 'Failed to extract user from SAML response');
+
+      const { username, mail } = user;
+
+      log(`${user.fullname} logging in using SSO-NFDI4Culture SAML strategy`);
+
+      const userdata = await (async () => {
+        const resolvedUser = await userCollection.findOne({ username, mail });
+        if (!resolvedUser) {
+          log(`${user.fullname} is not registered yet. Creating new account...`);
+          const insertResult = await userCollection.insertOne({
+            ...user,
+            _id: new ObjectId(),
+          });
+          if (!insertResult.acknowledged) {
+            return false;
+          }
+
+          const insertedUser = await userCollection.findOne({ _id: insertResult.insertedId });
+          if (!insertedUser) {
+            return false;
+          }
+          return insertedUser;
+        }
+        return resolvedUser;
+      })();
+      if (!userdata) return status(500, 'Failed to retrieve or create user');
+
+      const origin = request.headers.get('origin') ?? '';
+      const isLocalhost = origin.includes('localhost') || origin.includes('127.0.0.1');
+
+      auth.set({
+        value: await jwt.sign({ username: userdata.username, _id: userdata._id.toString() }),
+        path: '/',
+        httpOnly: true,
+        sameSite: isLocalhost ? 'none' : 'lax',
+        secure: isLocalhost ? true : undefined,
+      });
+
+      return redirect('/');
+    },
+    {
+      body: t.Object({
+        SAMLResponse: t.String(),
+      }),
+    },
+  );
 
 export default ssoNFDI4CultureRouter;
--- src/plugins/sso-nfdi4culture/saml.ts
+++ src/plugins/sso-nfdi4culture/saml.ts
@@ -1,26 +1,25 @@
-import { SAML } from '@node-saml/node-saml';
-import { UserRank } from 'src/common';
+import { type Profile } from '@node-saml/node-saml';
+import { UserRank, type IUserData } from 'src/common';
 import { err, log, warn } from 'src/logger';
 
-export const samlProfileToUser = (profile: any) => {
+export const samlProfileToUser = (profile: Profile) => {
   try {
     // TODO remove after debugging
     log('SAML Profile:', profile);
-    if (!profile.uid || !profile.mail || !profile.givenName || !profile.sn) {
+    const mail = profile.mail ?? profile.email;
+    if (!profile.uid || !mail || !profile.givenName || !profile.sn) {
       warn('Missing required fields from SAML response');
       return false;
     }
-    const adjustedUser = {
-      username: profile.uid,
-      fullname: profile.cn || `${profile.givenName} ${profile.sn}`,
-      prename: profile.givenName,
-      surname: profile.sn,
-      mail: profile.mail,
-      role: UserRank.user,
-      data: {
-        // ...getEmptyUserData(),
-      },
-      strategy: 'saml',
+    const adjustedUser: Omit<Omit<IUserData, 'sessionID'>, '_id'> & { strategy: string } = {
+      username: profile.uid.toString(),
+      fullname: profile.cn?.toString() ?? `${profile.givenName} ${profile.sn}`,
+      prename: profile.givenName.toString(),
+      surname: profile.sn.toString(),
+      mail: mail,
+      role: UserRank.uploader,
+      data: {},
+      strategy: 'sso-nfdi4culture',
     };
 
     log(`${adjustedUser.fullname} logging in using SAML strategy`);
